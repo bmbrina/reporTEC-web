f076888654163fa32f807aa3ffc0d9ae0baeb353
Barbara Brina <bmbrina9@gmail.com>
2017-11-14 16:56:16 -0600

    Calls alom from index js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { browserHistory } from 'react-router';
 import Routes from './routes';
 import './css/application.css';
+import './css/alom.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
